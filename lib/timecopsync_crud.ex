bf09f4956931599e895ebb506d340d21fe0a88b3
Yann POMIE <yann.pomie@laposte.net>
2023-06-16 16:54:32 +0200

    Initial commit

---
+++ lib/timecopsync_crud.ex
@@ -0,0 +1,18 @@
+defmodule TimecopsyncCrud do
+  @moduledoc """
+  Documentation for `TimecopsyncCrud`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TimecopsyncCrud.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/timecopsync_crud_test.exs
@@ -0,0 +1,8 @@
+defmodule TimecopsyncCrudTest do
+  use ExUnit.Case
+  doctest TimecopsyncCrud
+
+  test "greets the world" do
+    assert TimecopsyncCrud.hello() == :world
+  end
+end
