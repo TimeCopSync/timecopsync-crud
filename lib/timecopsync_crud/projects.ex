a83defed8c48bd2e455e1a431c84514c5ecd3fc1
WoodenMaiden <yann.pomie@laposte.net>
2023-11-12 22:11:30 +0100

    fix: problems with migration

--- lib/timecopsync_crud/projects.ex
+++ lib/timecopsync_crud/projects.ex
@@ -1,6 +1,7 @@
 defmodule TimecopsyncCrud.Projects do
   alias TimecopsyncCrud.Projects.Project
   alias TimecopsyncCrud.Repo
+  import Ecto.Query
 
   def get_project(id) do
     Repo.get(Project, id)
--- priv/repo/migrations/20231112003541_add_projects.exs
+++ priv/repo/migrations/20231112003541_add_projects.exs
@@ -2,8 +2,8 @@ defmodule TimecopsyncCrud.Repo.Migrations.AddProjects do
   use Ecto.Migration
 
   def change do
-    create table(:project) do
-      add :id, :string, primary_key: true, size: 36, null: false
+    create table(:project, primary_key: false) do
+      add :id, :uuid, primary_key: true, null: false
       add :name, :string, null: false
       add :colour, :integer, null: true
       add :archived, :boolean, null: false, default: false
